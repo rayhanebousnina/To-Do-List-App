702c3eab28a3186950f6e95ca18543a2eddaacd7
Rayhane <bousninarayhanesimplonenda@gmail.com>
2021-06-21 20:17:08 +0100

    blah blah

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [task, setTask] = useState("")
+  const handleChange = (e) => {
+    setTask(e.target.value)
+    console.log(task)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>To Do List</h1>
+      <input onChange={handleChange} placeholder="Enter your task"></input>
+      <ul>
+        <li>{task}</li>
+      </ul>
     </div>
   );
 }
